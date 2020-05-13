fc1106ea4d29b9792f7472ef0b4db499af8a8957
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-13 16:04:43 -0500

    have structure down and showing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react"; //importing useState
 import "./App.css";
+import axios from 'axios' //importing axios
 
 function App() {
+  const [data, setData] = useState(''); //setting variable data
+  axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=SfX7tbXZQ3LkdWIRUdV3sr8WkToUBhFz37lw2o28')
+    .then(response => {
+      console.log(response.data);
+      setData(response.data) //setting data to response.data
+    })
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        <h1>{data.title}</h1>
+        <h2>{data.date}</h2>
+        <img src={data.url} alt= {data.title}/>
+        <p>{data.explanation}</p>
+        
+        <button>{data.hdurl}</button>
       </p>
     </div>
   );
